24efaad9b5bc0591b8b1f38c6ff0a5b946c4f11b
薄浩宇 <123456>
2020-06-08 17:13:22 +0800

    1

--- xcx_workman/pages/showwork/showwork.js
+++ xcx_workman/pages/showwork/showwork.js
@@ -41,6 +41,7 @@ Page({
     // ],
     imgList:[],
     sousuotext:'',
+    id:1,
     sousuonr:''
   },
   onPullDownRefresh: function () {
@@ -51,6 +52,11 @@ Page({
     }, 1000);
   },
   onLoad: function (options) {
+    this.setData({
+      weizhi:app.globalData.weizhi
+    })
+    this.QueryoneArea()
+    this.QuerytwoArea()
     if(app.globalData.showid == 0){
       this.getShowList()
     }else{
@@ -269,5 +275,173 @@ Page({
         })
       }
     })
-  }
+  },
+  // 一级区域
+  QueryoneArea(){
+    var that = this
+    qingqiu.get("queryOneArea", null, function(re) {
+    if (re.success == true) {
+      if (re.result != null) {
+        that.city=re.result
+        that.setData({
+          city:that.city
+        })
+      }else {
+        qingqiu.tk('未查询到任何数据')
+      }
+    } 
+  })
+  },
+  // 二级区域
+  QuerytwoArea(){
+    var that = this
+    var data ={
+      oneAreaId:that.data.id
+    }
+    qingqiu.get("queryTwoArea", data, function(re) {
+    if (re.success == true) {
+      if (re.result != null) {
+        that.area=re.result
+        that.setData({
+          area:that.area
+        })
+      }else {
+        qingqiu.tk('未查询到任何数据')
+      }
+    } 
+  })
+  },
+  //地址 显示弹窗样式
+  showModal: function(e) {
+    this.setData({
+      hasMask: true
+    })
+    var animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.opacity(0).rotateX(-100).step();
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function() {
+      animation.opacity(1).rotateX(0).step();
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+
+
+  },
+  //隐藏弹窗样式 地址
+  hideModal: function() {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      hasMask: false
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+  cityyiji: function() {
+    var that = this
+    qingqiu.get("oneAreaService", {}, function(re) {
+      if (re.data.result.length > 0) {
+        that.setData({
+          cityId: re.data.result[0].id,
+          cityname1: re.data.result[0].areaName
+        })
+      }
+      that.setData({
+        city: re.data.result
+      })
+      that.cityerji()
+    })
+  },
+  cityerji: function() {
+    var that = this
+    var data = {
+      oneAreaId: that.data.cityId
+    }
+    qingqiu.get("getAllTwoArea", data, function(re) {
+      that.setData({
+        area: re.data.result
+      })
+    })
+  },
+  // 左侧按钮
+  cityleft: function(e) {
+    var that = this;
+    // var index = e.currentTarget.dataset.index;
+    var id = e.currentTarget.dataset.id
+    var name = e.currentTarget.dataset.name
+    that.setData({
+      cityId: id,
+      cityname1: name,
+    })
+    var data ={
+      oneAreaId:id
+    }
+    qingqiu.get("queryTwoArea", data, function(re) {
+      if (re.success == true) {
+        if (re.result != null) {
+          that.area=re.result
+          that.setData({
+            area:that.area
+          })
+        }else {
+          qingqiu.tk('未查询到任何数据')
+        }
+      } 
+    })
+  },
+  // 右侧单选点击
+  arearight: function(e) {
+    var that = this;
+    if(that.data.cityname1==undefined)
+    {
+      wx.showToast({
+        title: '请先选择城市',
+        icon:'none',
+        duration:2000
+      })
+      return
+    }
+    if(that.data.cityname1=='')
+    {
+      wx.showToast({
+        title: '请先选择城市',
+        icon:'none',
+        duration:2000
+      })
+      return
+    }
+    //var index = e.currentTarget.dataset.index;
+    var id = e.currentTarget.dataset.id
+    var name = e.currentTarget.dataset.name
+    getApp().globalData.weizhi = this.data.cityname1+name
+    that.setData({
+      weizhi:app.globalData.weizhi,
+      areaId: id,
+      //curIndex: index,
+      areaname: name,
+      showModalStatus: false,
+      cityname: this.data.cityname1
+    })
+  },
 })

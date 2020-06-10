03dae3332d4e59bb9af0bea42f2cb82553dee4a6
薄浩宇 <123456>
2020-06-10 09:08:10 +0800

    1

--- xcx_workman/pages/jsjg/jsjg.js
+++ xcx_workman/pages/jsjg/jsjg.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    jieguo:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.data.jieguo=options.obj
+    this.setData({
+      jieguo:this.data.jieguo
+    })
   },
 
   /**

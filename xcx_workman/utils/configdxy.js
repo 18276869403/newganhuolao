5cc3cb913d3cb7a487f5904651869856dbce6a7c
邓星玉 <1336797684@qq.com>
2020-06-30 10:00:42 +0800

    1

--- xcx_workman/utils/configdxy.js
+++ xcx_workman/utils/configdxy.js
@@ -1,6 +1,6 @@
 var api = require('./config.js')
 // const baseUrl = "http://miss.it-ys.com:91/work-boot/"  
-const baseUrl = "http://192.168.1.248:8080/work-boot/" 
+const baseUrl = "http://192.168.1.248:8080/work-boot/"  
 
 // 2.0需要工人，商家分类表：可选2个分类-编辑
 const WxUserAdd = baseUrl + "/pc/hall/wxUserAdd"

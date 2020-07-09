04f696cd95b30db49c431f34ac0e808f8c51e9f0
薄浩宇 <123456>
2020-07-09 18:56:55 +0800

    1

--- xcx_workman/pages/myWelfare/myWelfare.js
+++ xcx_workman/pages/myWelfare/myWelfare.js
@@ -1,11 +1,27 @@
 // pages/myWelfare/myWelfare.js
+const app = getApp()
+//调用接口js
+const qingqiu = require('../../utils/request.js')
+const api = require('../../utils/config.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    viewUrl: api.viewUrl,
+    isShowConfirm:false,
+    gongyilist:[{
+      id:1,
+      title:'七月敬老活动',
+      content:'于本月20日组织爱心人士前往万载养老院，针对老师做些基本护理工作，帮助老人打扫房间、清理敬老院；陪老人聊天、散步、锻炼身体；为老人奉上丰富多彩的文艺表演活动；进行各种各样的小活动。欢迎爱心人士踊跃报名',
+      time:'2020-7-20'
+    },{
+      id:2,
+      title:'七月敬老活动',
+      content:'关爱老人，关爱留守儿童',
+      time:'2020-7-20'
+    }],
   },
 
   /**
@@ -14,53 +30,19 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //切换
+  changeType: function(e) {
+    var that = this;
+    var id = e.currentTarget.dataset.id
+    that.setData({
+      needsTypeid: id
+    })
+    // this.getmyEmploy()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 公益详情
+  WelfareDetail:function(){
+    wx.navigateTo({
+      url: '../WelfareDetail/WelfareDetail',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
